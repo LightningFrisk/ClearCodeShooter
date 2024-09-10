c5c78c10672747ccc09c276a5af9e2ea980395f7
Caleb Cook <bmxcaleb@gmail.com>
2024-09-09 21:16:01 -0500

    spinning up project, first steps with GDscript

--- scenes/logo.gd
+++ scenes/logo.gd
@@ -1,11 +1,26 @@
 extends Sprite2D
 
+var pos: Vector2 = Vector2.ZERO
+const speed: int = 10
+
+var test_scale: int = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	position = Vector2(100,200)
+	pos = Vector2(300,200)
+	position = pos
+	
+	var testRotation = 45
+	rotation_degrees = testRotation
+	
+	test_scale = 2
+	scale = Vector2(test_scale, test_scale)
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	print('process')
+	pos.x += speed
+	position = pos
+	test_scale += 1
+	scale = Vector2(test_scale, test_scale)
