18a266025a75104d7277df31988b2af3a96f1dbf
Caleb Cook <bmxcaleb@gmail.com>
2024-09-10 16:16:37 -0500

    up to reading inputs, i have a little guy who moves around

--- scenes/logo.gd
+++ scenes/logo.gd
@@ -1,7 +1,7 @@
 extends Sprite2D
 
 var pos: Vector2 = Vector2.ZERO
-const speed: int = 10
+const speed: int = 200
 
 var test_scale: int = 1
 
@@ -10,17 +10,17 @@ func _ready() -> void:
 	pos = Vector2(300,200)
 	position = pos
 	
-	var testRotation = 45
-	rotation_degrees = testRotation
+	#var testRotation = 45
+	#rotation_degrees = testRotation
 	
-	test_scale = 2
+	test_scale = 1
 	scale = Vector2(test_scale, test_scale)
 
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pos.x += speed
+	pos.x += speed * delta
 	position = pos
-	test_scale += 1
-	scale = Vector2(test_scale, test_scale)
+	#test_scale += 1
+	#scale = Vector2(test_scale, test_scale)
---
+++ scenes/player/player.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+func _process(delta):
+	#input
+	var direction = Input.get_vector("left","right","up","down")
+	position += direction * 400 * delta
+	
+	#laser shooting input
+	if Input.is_action_pressed("primary action"):
+		print("Bang!")
+	
+	if Input.is_action_pressed("secondary action"):
+		print("Grenade!")
