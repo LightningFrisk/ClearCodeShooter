2c92ede54456d706dc7e5f808ed569270aa3088a
Caleb Cook <bmxcaleb@gmail.com>
2024-09-12 21:24:11 -0500

    part of physics, need to do 2nd part of challenge with drone

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,24 +1,8 @@
 extends Node2D
 
-var test_array: Array[String] = ["Hello", "World", "Stuff"]
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Car.rotation_degrees = 45
-	
-	#for i in test_array:
-		#print(i)
-	#
-	#print(test_array[0])
+	return
 
-func _process(delta):
-	$Car.rotation_degrees += 40 * delta
-		
-	if $Car.position.x > 1000:
-		print('position exceeded')
-		$Car.pos.x = 0 
-		
-	
-	#print(Input.is_action_pressed("left"))
-
-func test_function():
-	print("This is a test function")
+func _process(_delta):
+	return
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,11 +1,10 @@
-extends Node2D
+extends CharacterBody2D
 
-var testVariable = "Hello World!"
-
-func _process(delta):
+func _process(_delta):
 	#input
 	var direction = Input.get_vector("left","right","up","down")
-	position += direction * 400 * delta
+	velocity = direction * 500
+	move_and_slide()
 	
 	#laser shooting input
 	if Input.is_action_pressed("primary action"):
