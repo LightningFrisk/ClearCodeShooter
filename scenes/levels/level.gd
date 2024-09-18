50c6811d83a9716ef8d9945a0d031096c172644c
Caleb Cook <bmxcaleb@gmail.com>
2024-09-18 16:30:10 -0500

    we're shootin lasers

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var laser_scene: PackedScene = preload("res://scenes/projectiles/laser.tscn")
+
 func _on_gate_player_entered_gate(body):
 	print("player has entered gate")
 	print(body)
@@ -10,4 +12,10 @@ func _on_player_grenade_has_fired() -> void:
 
 
 func _on_player_laser_has_fired() -> void:
-	print("player has fired laser")
+	var laser = laser_scene.instantiate()
+	
+	#TODO UPDATE LASER POSITION
+	laser.position = $Player.position
+	#TODO MOVE THE LASER
+	#TODO ADD LASER TO NOTE 2D
+	add_child(laser)
