56e137dcd96fa684bf4f52627254587d6be2116f
Caleb "Trip" Cook <68445331+LightningFrisk@users.noreply.github.com>
2024-10-30 21:37:10 -0500

    laser does stuff now

--- scenes/enemies/drone.gd
+++ scenes/enemies/drone.gd
@@ -1,8 +1,13 @@
 extends CharacterBody2D
 
+#var is_enemy: bool = true;
+
 func _process(_delta):
 	var direction = Vector2.RIGHT
 	
 	velocity = direction * 400
 	
 	move_and_slide()
+	
+func hit():
+	print("hit")
--- scenes/projectiles/laser.gd
+++ scenes/projectiles/laser.gd
@@ -3,5 +3,19 @@ extends Area2D
 @export var speed: int = 1000
 var direction: Vector2 = Vector2.UP
 
+func _ready() -> void:
+	$SelfDestructTimer.start()
+
 func _process(delta: float) -> void:
 	position += direction * speed * delta
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if "hit" in body:
+		body.hit()
+	#if "is_enemy" in body:
+		#print("is an enemy")
+	queue_free()
+
+func _on_self_destruct_timer_timeout() -> void:
+	queue_free()
